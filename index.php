66ac914adb29d11a2f7e59fc51f31d336cd7b019
ICT170 <inoshharsha531@gmail.com>
2022-01-11 12:19:13 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="details.php" method="POST">
+
+            <p>Name : <input type="text" name="name" id="name"></p><br>
+            <p>Reg No: <input type="text" name="reg" id="reg"></p><br>
+            <input type="submit" name="submit" id="submit" value="submit">
+
+    </form>
+
+    
+
+
+</body>
+
+
+</html>
